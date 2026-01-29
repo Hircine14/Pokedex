837c949496013bd04e95f3caf5198cdba17ea7a6
unknown <mathisvallejos@gmail.com>
2026-01-29 17:55:32 +0100

    update: local data

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//DOM Call test
+//DOM Call
 const input = document.getElementById('pokemon-input');
 const searchBtn = document.getElementById('search-btn');
 const mainScreen = document.getElementById('main-screen');
@@ -6,5 +6,5 @@ const recommendationsContainer = document.getElementById('recommendations');
 const historyContainer = document.getElementById('history');
 const clearHistoryBtn = document.getElementById('clear-history');
 const refreshRecommendationsBtn = document.getElementById('refresh-recommendations');
-///test
+
 
