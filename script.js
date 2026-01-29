a18a42580e2a30da1115b8485c999687039b4a71
unknown <mathisvallejos@gmail.com>
2026-01-29 15:31:34 +0100

    Backend

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const input = document.getElementById('pokemon-input');
+const searchBtn = document.getElementById('search-btn');
+const mainScreen = document.getElementById('main-screen');
+const recommendationsContainer = document.getElementById('recommendations');
+const historyContainer = document.getElementById('history');
+const clearHistoryBtn = document.getElementById('clear-history');
+const refreshRecommendationsBtn = document.getElementById('refresh-recommendations');
+
